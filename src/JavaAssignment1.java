fdcfc61592b82ad0627451e1fbf31ed77ed1eda5
Yuta <yutanagasakaaa@gmail.com>
2018-07-16 16:37:28 -0400

    JavaAssignment1
    Completed

---
+++ src/JavaAssignment1.java
@@ -0,0 +1,17 @@
+public class JavaAssignment1 {
+	public static void main(String[] args) {
+		String firstName = "Yuta";
+		String lastName = "Nagasaka";
+		System.out.println(firstName + "\r" + lastName);
+		System.out.println(firstName + " " + lastName);
+		System.out.println(lastName + ", " + firstName);
+		System.out.println(firstName);
+		System.out.println(lastName);
+		System.out.println(firstName.charAt(2));
+		System.out.println(lastName.charAt(1));
+		System.out.print(firstName.charAt(0));
+		System.out.println(firstName.charAt(1));
+		System.out.print(firstName.charAt(1));
+		System.out.println(lastName.charAt(2));
+	}
+}
